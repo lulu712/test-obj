c36b58a886506017ea307c6ba654d2797fc1a7df
ㄧlulu <benz9779@gmail.com>
2026-02-17 17:43:25 +0800

    第二次提交

--- src/main.js
+++ src/main.js
@@ -1,15 +1,5 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
-import router from './router'
 
-
-const app = createApp(App)
-app.use(ElementPlus)
-
-//一定要先掛載router，才能在組件中使用router.push()等方法
-app.use(router)
-app.mount('#app')
-
+createApp(App).mount('#app')
