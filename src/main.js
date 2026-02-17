84d0a05434e0ea69bb802817867e037f2f86cb48
ㄧlulu <benz9779@gmail.com>
2026-02-17 17:53:35 +0800

    Revert "第二次提交"
    This reverts commit c36b58a886506017ea307c6ba654d2797fc1a7df.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,15 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import router from './router'
 
-createApp(App).mount('#app')
+
+const app = createApp(App)
+app.use(ElementPlus)
+
+//一定要先掛載router，才能在組件中使用router.push()等方法
+app.use(router)
+app.mount('#app')
+
